976f033f7dee452e83d127ef99bd3e21293a9c4a
PRUDHVI <prudhvichowdary14@gmail.com>
2024-08-31 15:40:10 +0530

    added scoreboard project

---
+++ scoreboard/index.js
@@ -0,0 +1,42 @@
+function func_home_1() {
+    
+    let currentScore = document.getElementById("score-board-home").textContent;
+    currentScore = parseInt(currentScore)
+    document.getElementById("score-board-home").textContent = currentScore+1;
+}
+
+function func_home_3() {
+    
+    let currentScore = document.getElementById("score-board-home").textContent;
+    currentScore = parseInt(currentScore)
+    document.getElementById("score-board-home").textContent = currentScore+3;
+}
+
+function func_home_5() {
+    
+    let currentScore = document.getElementById("score-board-home").textContent;
+    currentScore = parseInt(currentScore)
+    document.getElementById("score-board-home").textContent = currentScore+5;
+}
+
+
+function func_guest_1() {
+    
+    let currentScore = document.getElementById("score-board-guest").textContent;
+    currentScore = parseInt(currentScore)
+    document.getElementById("score-board-guest").textContent = currentScore+1;
+}
+
+function func_guest_3() {
+    
+    let currentScore = document.getElementById("score-board-guest").textContent;
+    currentScore = parseInt(currentScore)
+    document.getElementById("score-board-guest").textContent = currentScore+3;
+}
+
+function func_guest_5() {
+    
+    let currentScore = document.getElementById("score-board-guest").textContent;
+    currentScore = parseInt(currentScore)
+    document.getElementById("score-board-guest").textContent = currentScore+5;
+}
