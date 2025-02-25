26b46ba3ea0ba6541dd17ad6c454abcca085bfff
PRUDHVI <prudhvichowdary14@gmail.com>
2025-02-25 23:51:14 +0530

    added js solo and leads tracker app

---
+++ js_solo/index.js
@@ -0,0 +1,15 @@
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passcode1 = document.getElementById("password-1")
+let passcode2 = document.getElementById("password-2")
+
+function gen_password() {
+    passcode1.textContent = "wdhwidh"
+    passcode2.textContent = "wdhwidh"
+}
+
+function gen_random_password() {
+
+
+}
